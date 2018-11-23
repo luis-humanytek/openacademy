33963d1750432952dfe2c965fe64b50488e76a19
humanytek <luis-humanytek>
2018-11-23 17:12:10 -0600

    [ADD]add files module

---
+++ __manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'openacademy',
+    'author': 'Humanytek',
+    'website': 'http://www.humanytek.com',
+    'category': 'Uncategorized',
+    'version': '1.0.0',
+    'depends': [
+    ],
+    'data': [
+#        'security/security.xml',
+#        'security/ir.model.access.csv',
+#        'reports/session.xml',
+#        'views/course.xml',
+#        'views/partner.xml',
+#        'views/session.xml',
+#        'views/wizard.xml',
+    ],
+}
